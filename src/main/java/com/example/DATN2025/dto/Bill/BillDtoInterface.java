f2ec5f443c7eb1e5381de95e0c4ff63a3d1fd17a
SonDuaaa <sonqpal2005@gmail.com>
2025-11-05 00:18:36 +0700

    Hoàn thành chức năng đổi trả, sửa lỗi cú pháp template

--- src/main/java/com/example/DATN2025/dto/Bill/BillDtoInterface.java
+++ src/main/java/com/example/DATN2025/dto/Bill/BillDtoInterface.java
@@ -1,10 +1,10 @@
 package com.example.DATN2025.dto.Bill;
 
-
 import com.example.DATN2025.entity.enumClass.BillStatus;
 import com.example.DATN2025.entity.enumClass.InvoiceType;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 public interface BillDtoInterface {
     Long getMaHoaDon();
