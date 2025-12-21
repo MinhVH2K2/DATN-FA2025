e64d7d9ce624b1b0e1193097dfb1893b673480c7
NgHaiVan <vannhph52137@gmail.com>
2025-12-21 13:30:46 +0700

    Add files via upload

---
+++ AddressShippingRepository.java
@@ -0,0 +1,11 @@
+package com.example.DATN2025.repository;
+
+import com.example.DATN2025.entity.AddressShipping;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.support.QuerydslJpaRepository;
+
+import java.util.List;
+
+public interface AddressShippingRepository  extends JpaRepository<AddressShipping, Long> {
+    List<AddressShipping> findAllByCustomer_Account_Id(Long accountId);
+}
